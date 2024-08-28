fe3d1266a917c0a7fe3f805209d1ea7531b3a531
jshep23prog <jshep23@gmail.com>
2024-08-28 12:56:20 -0500

    TTS indiviudual flies. essentially an book reader

--- demo.py
+++ demo.py
@@ -1,8 +1,14 @@
 from gtts import gTTS
 import os
 
-text ="James is the best"
-output = gTTS(text=text, lang='en', slow=False)
-output.save('output.mp3')
+# text ="James is the best"
+# output = gTTS(text=text, lang='en', slow=False)
+# output.save('output.mp3')
 
-os.system("afplay output.mp3")
+# os.system("afplay output.mp3")
+
+text = open('demo.txt', 'r').read()
+language='en'
+output = gTTS(text=text, lang=language, slow=False)
+output.save('fileoutput.mp3')
+os.system("afplay fileoutput.mp3")
